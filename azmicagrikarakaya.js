0490e5072009fe7e0de619ee5048696af4f48b70
azmick <118058488+azmick@users.noreply.github.com>
2025-09-09 19:18:06 +0300

    first commit

---
+++ azmicagrikarakaya.js
@@ -0,0 +1,10 @@
+((function(){
+    const init = () => {
+        console.log('sayfa yükleniyor...');
+        //veri yükleme fonksiyonu
+        //html'i oluşturma fonksiyonu
+        //css'i oluşturma fonksiyonu
+        //event listener ekleme fonksiyonu
+    }
+    init();
+})());
